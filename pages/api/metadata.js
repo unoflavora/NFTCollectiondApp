e7d8217a5f18b0377fbab539a41e44cb81082b16
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2022-08-01 05:42:57 +0700

    debugging ;

--- pages/api/metadata.js
+++ pages/api/metadata.js
@@ -1,9 +1,13 @@
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
+  const altTokenId = req.query.token_id
+  const otherAlt = req.body.token_id
+  const otherAlt2 = req.body.tokenId
+  console.log(tokenId, altTokenId, otherAlt, otherAlt2)
   const image_url = `https://raw.githubusercontent.com/unoflavora/nftagaterndimage/main/%20(${tokenId}).png`
   res.status(200).json({
     name: "Agate Dev NFT #" + tokenId,
-    description: "Wey dapet NFT nih. idnya :" + tokenId,
+    description: "Debug, URL: " + image_url + 'ALt' + altTokenId, otherAlt, otherAlt2,
     image: image_url
   });
 }
